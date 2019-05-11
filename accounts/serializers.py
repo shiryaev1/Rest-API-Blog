593d15dbeab8d5c4e226da4c98cd16eeb9eae165
Your Name <shiryaevv1@yandex.by>
2019-05-11 14:06:49 +0300

    Serializer Validation

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,25 +1,55 @@
 from django.contrib.auth import get_user_model
 
-
 from rest_framework.serializers import (
-    ModelSerializer
+    ModelSerializer,
+    EmailField,
+    ValidationError
     )
 
 User = get_user_model()
 
 
 class UserCreateSerializer(ModelSerializer):
+    email = EmailField(label='Email Address')
+    email2 = EmailField(label='Confirm Email')
 
     class Meta:
         model = User
         fields = [
             'username',
             'email',
+            'email2',
             'password'
         ]
         extra_kwargs = {'password':
                             {'write_only': True}
                             }
+
+    def validate(self, data):
+        # email = data['email']
+        # user_qs = User.objects.filter(email=email)
+        # if user_qs.exists():
+        #     raise ValidationError('This user has already registered.')
+        return data
+
+    def validate_email(self, value):
+        data = self.get_initial()
+        email1 = data.get('email2')
+        email2 = value
+        if email1 != email2:
+            raise ValidationError('Emails must match.')
+        user_qs = User.objects.filter(email=email2)
+        if user_qs.exists():
+            raise ValidationError('This user has already registered.')
+        return value
+
+    def validate_email2(self, value):
+        data = self.get_initial()
+        email1 = data.get('email')
+        email2 = value
+        if email1 != email2:
+            raise ValidationError('Emails must match.')
+        return value
 
     def create(self, validated_data):
         username = validated_data['username']
