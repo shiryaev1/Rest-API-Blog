10e73da26ed22915183bb7975f40f08f4231a572
Your Name <shiryaevv1@yandex.by>
2019-05-11 12:48:27 +0300

    ModelSerializer Create Method

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,14 +1,9 @@
-from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth import get_user_model
 
 
 from rest_framework.serializers import (
-    ModelSerializer,
-    HyperlinkedIdentityField,
-    SerializerMethodField,
-    ValidationError
+    ModelSerializer
     )
-
 
 User = get_user_model()
 
@@ -19,12 +14,24 @@ class UserCreateSerializer(ModelSerializer):
         model = User
         fields = [
             'username',
-            'password',
-            'email'
+            'email',
+            'password'
         ]
         extra_kwargs = {'password':
                             {'write_only': True}
                             }
+
+    def create(self, validated_data):
+        username = validated_data['username']
+        email = validated_data['email']
+        password = validated_data['password']
+        user_obj = User(
+                username=username,
+                email=email
+            )
+        user_obj.set_password(password)
+        user_obj.save()
+        return validated_data
 
 
 
